98b54e9516a471d4831f11cdd3b8fdc0653b5e07
Ali Hassan Kazmi <Ali.Kazmi@tpsonline.com>
2023-11-20 12:43:24 +0500

    Add a driving license criteria program in python

---
+++ drivingLicense.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+
+if age > 17 and age < 60: 
+    print("You are allowed to get driving license.")
+else:
+    print("Under 18 or senior citizens are not allowed to get license.")
